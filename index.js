2abf732dae7b9ce0b4b35c07b483af80cf5c9e5a
Ajay Raj Tiwari <ajay.tiwari@sociolla.com>
2024-05-26 11:56:43 +0530

    initial

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB CONNETED "))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend Ser running ");
+});

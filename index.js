5bff6c77959356c2e4593201da2d2d0fde2e2951
Ajay Raj Tiwari <ajay.tiwari@sociolla.com>
2024-07-14 16:25:15 +0530

    crrated ecommerce API

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripRoute = require("./routes/stripe");
+const cors = require("cors");
+
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB CONNETED "))
+  .then(() => console.log("DB CONNETED"))
   .catch((err) => console.log(err));
-
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripRoute);
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend Ser running ");
 });
